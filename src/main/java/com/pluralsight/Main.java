a07d85e688d459e35991e45fd30cfc88378ac060
SidiqiObaid <obaid.yearuo@gmail.com>
2024-10-13 17:02:07 -0400

    main menu is completed

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,29 +3,57 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("\n-----------------------------------------------------------------------");
         System.out.println("----------     Welcome to Accounting Ledger Application!     ----------");
+        System.out.println("-----------------------------------------------------------------------\n");
+        appHomeScreen();
+        boolean exitApp = true;
+
+
+        while (exitApp){
+        char command = Utilities.PromptForStringAndGetChar("Please Select From: [D], [P], [L], [X] :  ");
+        switch (command){
+
+            case 'D':
+                addDeposit();
+                break;
+            case 'P':
+                addPayment();
+                break;
+            case 'L':
+                ledgerMenu();
+                break;
+            case 'X':
+                    System.out.println("Thank you for choosing Us! ");
+                    exitApp = false;
+                    break;
+            default:
+                System.out.println("*** ERROR! ***  Invalid Selection: Please Select From: [D], [P], [L], [X] : ");
+
+        }
+        }
+
 
 
     }
     // Top level menu,
     static void appHomeScreen(){
-
-        System.out.println("\n --- Home Screen ---\n");
+       // System.out.println("-----------------------------------------------------------------------\n");
+        System.out.println("--------------- This is Your Home Screen Please Select ----------------");
         System.out.println("D)      Add Deposit");
         System.out.println("P)      Make Payment (Debit)");
         System.out.println("L)      Show Ledger");
-        System.out.println("X)      Exit Application");
+        System.out.println("X)      Exit Application\n");
     }
 
     // Method to add Deposit
-    static void appDeposit(){
+    static void addDeposit(){
         System.out.println("This is your Deposit Menu: \n");
     }
 
     // Method to make Payment
-    static void appLegerMenu(){
-        System.out.println("This is your Ledger Menu: \n");
+    static void addPayment(){
+        System.out.println("This is your Payment Menu: \n");
     }
 
     //Method to display Ledger
--- src/main/java/com/pluralsight/Utilities.java
+++ src/main/java/com/pluralsight/Utilities.java
@@ -8,24 +8,22 @@ public class Utilities {
 
     public static String PromptForString(String prompt){
         System.out.print(prompt);
-        return scanner.nextLine();
+        String input = scanner.nextLine().toUpperCase();
+        return input;
     }
+
+    public static char PromptForStringAndGetChar(String prompt){
+        System.out.print(prompt);
+        String input = scanner.nextLine().trim().toUpperCase();
+        char chr = input.charAt(0);
+        return chr;
+    }
+
+
+
 
     public static String PromptForString(){
         return scanner.nextLine();
-    }
-
-    public static boolean PromptForYesNo(String prompt){
-        System.out.print(prompt + " ( Y for Yes, N for No ) ?");
-        String userinput = scanner.nextLine();
-
-        return
-                (
-                        userinput.equalsIgnoreCase("Y")
-                                ||
-                                userinput.equalsIgnoreCase("YES")
-                );
-
     }
 
     public static short PromptForShort(String prompt){
@@ -55,4 +53,17 @@ public class Utilities {
         double userinput = Double.parseDouble(userInputs);
         return userinput;
     }
+
+public static boolean PromptForYesNo(String prompt){
+    System.out.print(prompt + " ( Y for Yes, N for No ) ?");
+    String userinput = scanner.nextLine();
+
+    return
+            (
+                    userinput.equalsIgnoreCase("Y")
+                            ||
+                            userinput.equalsIgnoreCase("YES")
+            );
+
+}
 }
