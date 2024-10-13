df2975ca4d8374af82fed13327ff1c64b9845e50
SidiqiObaid <obaid.yearuo@gmail.com>
2024-10-13 15:31:30 -0400

    the main structure of app is created

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,14 +4,54 @@ package com.pluralsight;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("----------     Welcome to Accounting Ledger Application!     ----------");
+
+
     }
+    // Top level menu,
+    static void appHomeScreen(){
+
+        System.out.println("Add Deposit");
+        System.out.println("Make Payment (Debit)");
+        System.out.println("Show Ledger");
+        System.out.println("Exit form ");
+    }
+
+    // Deposit Menu,
+    static void appDepositMenu(){
+        System.out.println("This is your Deposit Menu: \n");
+
+    }
+
+    //Payment Menu,
+    static void appLegerMenu(){
+        System.out.println("This is your Ledger Menu: \n");
+    }
+
+    //Ledger Menu
+    static void ledgerMenu(){
+        System.out.println("This is your Ledger Menu: \n");
+    }
+
+    //Ledger - Display all Entries
+    static void displayAllEntriesLedgers(){
+        System.out.println("All the Entries: ");
+    }
+
+    // Display only Deposits
+    static void displayDepositsLedger(){
+        System.out.println("All Deposits: ");
+    }
+
+    // Display only Payments
+    static void displayPaymentsLedger(){
+        System.out.println("All Deposits: ");
+    }
+
+    // Display pre-defined  Reports
+    static void displayReportsLedger(){
+        System.out.println("All Deposits: ");
+    }
+
 }
